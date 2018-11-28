3f93616ac23825d13d164d9d471bd0f5664c1c5b
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2018-11-27 19:10:29 -0700

    Finish MemberAccess cleanup

--- John.SocialClub.Data/DataAccess/ClubMemberAccess.cs
+++ John.SocialClub.Data/DataAccess/ClubMemberAccess.cs
@@ -17,33 +17,23 @@ namespace John.SocialClub.Data.DataAccess
         public DataTable GetAllClubMembers()
         {
             var dataTable = new DataTable();
-
             using (var dataAdapter = new SqlDataAdapter(Scripts.SqlGetAllClubMembers, ConnectionString))
             {
-
-                // Fill the datatable from adapter
                 dataAdapter.Fill(dataTable);
             }
-
             return dataTable;
         }
 
         public DataRow GetClubMemberById(int id)
         {
             var dataTable = new DataTable();
-
             using (var dataAdapter = new SqlDataAdapter(Scripts.sqlGetClubMemberById, ConnectionString))
             {
-            DataRow dataRow;
-                // Add the parameter to the parameter collection
                 dataAdapter.SelectCommand.Parameters.AddWithValue("@Id", id);
-
-                // Fill the datatable From adapter
                 dataAdapter.Fill(dataTable);
-
-                // Get the datarow from the table
-                dataRow = dataTable.Rows.Count > 0 ? dataTable.Rows[0] : null;
-
+                DataRow dataRow = dataTable.Rows.Count > 0 
+                    ? dataTable.Rows[0] 
+                    : null;
                 return dataRow;
             }
         }
@@ -51,27 +41,12 @@ namespace John.SocialClub.Data.DataAccess
         public DataTable SearchClubMembers(object occupation, object maritalStatus, string operand)
         {
             var dataTable = new DataTable();
-
-            using (var sqlDataAdapter = new SqlDataAdapter())
+            using (var sqlDataAdapter = new SqlDataAdapter(string.Format(Scripts.SqlSearchClubMembers, operand), ConnectionString))
             {
-                // Create the command and set its properties
-                sqlDataAdapter.SelectCommand = new SqlCommand
-                {
-                    Connection = new SqlConnection(ConnectionString),
-                    CommandType = CommandType.Text,
-
-                    // Assign the SQL to the command object
-                    CommandText = string.Format(Scripts.SqlSearchClubMembers, operand)
-                };
-
-                // Add the input parameters to the parameter collection
                 sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@Occupation", occupation == null ? DBNull.Value : occupation);
                 sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@MaritalStatus", maritalStatus == null ? DBNull.Value : maritalStatus);
-
-                // Fill the table from adapter
                 sqlDataAdapter.Fill(dataTable);
             }
-
             return dataTable;
         }
 
@@ -79,12 +54,10 @@ namespace John.SocialClub.Data.DataAccess
         {
             using (var sqlCommand = new SqlCommand())
             {
-                // Set the command object properties
                 sqlCommand.Connection = new SqlConnection(ConnectionString);
                 sqlCommand.CommandType = CommandType.Text;
                 sqlCommand.CommandText = Scripts.SqlInsertClubMember;
 
-                // Add the input parameters to the parameter collection
                 sqlCommand.Parameters.AddWithValue("@FirstName", clubMember.FirstName);
                 sqlCommand.Parameters.AddWithValue("@MiddleName", clubMember.MiddleName);
                 sqlCommand.Parameters.AddWithValue("@LastName", clubMember.LastName);
@@ -95,9 +68,8 @@ namespace John.SocialClub.Data.DataAccess
                 sqlCommand.Parameters.AddWithValue("@Salary", clubMember.Salary);
                 sqlCommand.Parameters.AddWithValue("@NumberOfChildren", clubMember.NumberOfChildren);
 
-                // Open the connection, execute the query and close the connection
                 sqlCommand.Connection.Open();
-                var rowsAffected = sqlCommand.ExecuteNonQuery();
+                int rowsAffected = sqlCommand.ExecuteNonQuery();
                 sqlCommand.Connection.Close();
 
                 return rowsAffected > 0;
@@ -108,12 +80,10 @@ namespace John.SocialClub.Data.DataAccess
         {
             using (var dbCommand = new SqlCommand())
             {
-                // Set the command object properties
                 dbCommand.Connection = new SqlConnection(ConnectionString);
                 dbCommand.CommandType = CommandType.Text;
                 dbCommand.CommandText = Scripts.sqlUpdateClubMember;
 
-                // Add the input parameters to the parameter collection
                 dbCommand.Parameters.AddWithValue("@FirstName", clubMember.FirstName);
                 dbCommand.Parameters.AddWithValue("@MiddleName", clubMember.MiddleName);
                 dbCommand.Parameters.AddWithValue("@LastName", clubMember.LastName);
@@ -125,9 +95,8 @@ namespace John.SocialClub.Data.DataAccess
                 dbCommand.Parameters.AddWithValue("@NumberOfChildren", clubMember.NumberOfChildren);
                 dbCommand.Parameters.AddWithValue("@Id", clubMember.Id);
 
-                // Open the connection, execute the query and close the connection
                 dbCommand.Connection.Open();
-                var rowsAffected = dbCommand.ExecuteNonQuery();
+                int rowsAffected = dbCommand.ExecuteNonQuery();
                 dbCommand.Connection.Close();
 
                 return rowsAffected > 0;
@@ -138,17 +107,14 @@ namespace John.SocialClub.Data.DataAccess
         {
             using (var dbCommand = new SqlCommand())
             {
-                // Set the command object properties
                 dbCommand.Connection = new SqlConnection(ConnectionString);
                 dbCommand.CommandType = CommandType.Text;
                 dbCommand.CommandText = Scripts.sqlDeleteClubMember;
 
-                // Add the input parameter to the parameter collection
                 dbCommand.Parameters.AddWithValue("@Id", id);
 
-                // Open the connection, execute the query and close the connection
                 dbCommand.Connection.Open();
-                var rowsAffected = dbCommand.ExecuteNonQuery();
+                int rowsAffected = dbCommand.ExecuteNonQuery();
                 dbCommand.Connection.Close();
 
                 return rowsAffected > 0;
